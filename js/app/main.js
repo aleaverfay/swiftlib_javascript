2c14e5d92236a6a5483ea59e8b7255a51aa738c8
Tim Jacobs <timjacobs2@gmail.com>
2013-11-29 16:30:45 -0500

    Implement table drag and drop. Fix table to csv. Minor stylistic changes

--- js/app/main.js
+++ js/app/main.js
@@ -1,6 +1,12 @@
 $(document).ready(function () {
 	//Setup the tabbed input interface
-	$( "#tabs" ).tabs();
+	$( "#tabs" ).tabs({
+		beforeActivate: function( event, ui ) {
+			if(ui.newPanel.attr('id') == 'csv_tab'){
+				handle_tab_vs_csv_change(this);
+			}
+		}
+	});
 
 	//Roll the instructions into a collapsable layout
 	$( "#accordion" ).accordion({
@@ -10,14 +16,23 @@ $(document).ready(function () {
 	//Add themable tooltips
 	$( document ).tooltip();
 
-	$( "button" ).button();
+	//And themable buttons
+	$( "input[type=submit], button" ).button();
+
+	//Playing around with validation, not working right now
+	//$( "#mainForm" ).validate({
+	//	errorLabelContainer: '#mainErrors'
+	//});
+
+	$( '#aacounts' ).dragtable({
+		dragaccept:'.accept'
+	});
 
 	$('#add').click( add_column_to_aacounts );
 	$('#delcol').click( delete_column_from_aacounts );
 	$('.aacountcell').blur( function() {validate_aacount_cell(this);} );
 	$('.seqposcell').blur( function() { validate_seqpos_cell(this);} );
 	$('#launchbutton').click( function() { validate_inputs_and_launch(this); } );
-	$("#tab_vs_csv_radio input:radio").click( function() { handle_tab_vs_csv_change(this); } );
 	$("#table_from_csv").click( populate_table_from_csv );
 });
 
